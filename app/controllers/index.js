42edb1c4e06f8eaf718498eca444b31f61bb0f6f
William Chandler <chndlr.wllm@gmail.com>
2018-08-03 22:53:22 -0500

    created admin page, route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -14,12 +14,12 @@ export default Controller.extend({
 
 		saveInvitation() {
 			const email = this.get('emailAddress');
-			const newInvitation = this.store.createRecord('invitation', { email: email });
-			newInvitation.save();
-			
-			this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-			this.set('emailAddress', '');
+			const newInvitation = this.store.createRecord('invitation', { email });
+
+            newInvitation.save().then(response => {
+                this.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
+                this.set('emailAddress', '');
+            });
 		}
 	}
-	
 });
