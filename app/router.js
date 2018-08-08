8bb44004676f7b97bba53afd8e301102a0f39f8b
William Chandler <chndlr.wllm@gmail.com>
2018-08-08 14:25:08 -0500

    new library model, templates

--- app/router.js
+++ app/router.js
@@ -2,17 +2,19 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('contact');
-
-  this.route('admin', function() {
-    this.route('invitations');
-  });
+	this.route('about');
+	this.route('contact');
+	this.route('admin', function() {
+		this.route('invitations');
+	});
+	this.route('libraries', function() {
+		this.route('new');
+	});
 });
 
 export default Router;
