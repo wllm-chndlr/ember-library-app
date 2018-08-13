b6d6e57cdc172d7634502b8c1ccdb4482ebe443e
William Chandler <chndlr.wllm@gmail.com>
2018-08-12 20:47:11 -0500

    created library edit page

--- app/router.js
+++ app/router.js
@@ -1,20 +1,26 @@
+// app/router.js
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-	location: config.locationType,
-	rootURL: config.rootURL
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-	this.route('about');
-	this.route('contact');
-	this.route('admin', function() {
-		this.route('invitations');
-	});
-	this.route('libraries', function() {
-		this.route('new');
-	});
+
+  this.route('about');
+  this.route('contact');
+
+  this.route('admin', function() {
+    this.route('invitations');
+    this.route('contacts');
+  });
+
+  this.route('libraries', function() {
+    this.route('new');
+    this.route('edit', { path: '/:library_id/edit' });
+  });
 });
 
 export default Router;
---
+++ app/routes/libraries/edit.js
@@ -0,0 +1,27 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+	model(params) {
+		return this.store.findRecord('library', params.library_id);
+	},
+
+	actions: {
+
+		saveLibrary(library) {
+			library.save().then(() => this.transitionTo('libraries'));
+		},
+
+		willTransition(transition) {
+			let model = this.controller.get('model');
+			if (model.get('hasDirtyAttributes')) {
+				let confirmation = confirm('Your changes have not saved yet. Would you like to leave this form?');
+				if (confirmation) {
+					model.rollbackAttributes();
+				} else {
+					transition.abort();
+				}
+			}
+		}
+	}
+});
